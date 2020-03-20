5fd2fde1d2798131465778abaddee3aa7a86867b
JoshuaSkootsky <joshua.skootsky@gmail.com>
2020-03-20 01:32:17 -0400

    Added a test for project
    Currently test fails. I will need to look at the project code and see if it has been hooked up to the DB
    If not, that is why this test fails.

---
+++ server/db/models/project.spec.js
@@ -0,0 +1,28 @@
+/* global describe beforeEach it */
+
+const { expect } = require('chai');
+const db = require('../index');
+const Project = db.model('project'); // get project off the DB
+
+describe('Project model', () => {
+  beforeEach(() => {
+    return db.sync({ force: true });
+  });
+
+  describe('simple properties', () => {
+    describe('has a title', () => {
+      let algoBeginner;
+      const titleString = 'Your first algo training website';
+
+      beforeEach(async () => {
+        algoBeginner = await Project.create({
+          title: titleString,
+        });
+      });
+
+      it('returns true if project exists in DB and has a title', () => {
+        expect(algoBeginner.title.to.be.equal(titleString));
+      });
+    }); // end describe('has a title')
+  }); // end describe('simple properties')
+}); // end describe('Project model')
