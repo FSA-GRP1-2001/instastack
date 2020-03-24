e0cd5a70709cdb457581915a920d35e8d8736c58
Amantina Sosa <amantinasosa@gmail.com>
2020-03-24 12:28:43 -0400

    added more components to seed file

--- server/db/models/component.js
+++ server/db/models/component.js
@@ -6,6 +6,13 @@ const tagConstants = {
   ul: { open: '<ul>', close: '</ul>' },
   li: { open: '<li>', close: '</li>' },
   div: { open: '<div>', close: '</div>' },
+  header: { open: '<header> <h1>', close: '</h1></header>' },
+  img: { open: '<img src="', close: '">' },
+  button: { open: '<button type="button">', close: '</button>' },
+  // navbar: { open: '<div>', close: '</div>' },
+  footer: { open: '<footer><p>', close: '</p></footer>' },
+  // form: { open: '<div>', close: '</div>' },
+  // twoColumnText: { open: '<div>', close: '</div>' },
 };
 
 const Component = db.define('component', {
