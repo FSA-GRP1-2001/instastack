7c114b9d5ea72ea9edc78816957ae0e5f4066fd6
JoshuaSkootsky <joshua.skootsky@gmail.com>
2020-03-25 17:57:58 -0400

    fix(webpack): makes webpack able to import React Prime components and assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
@@ -21,12 +23,16 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
         loader: 'babel-loader',
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|gif|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000',
       },
     ],
   },
