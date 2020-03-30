9de2f0c0100332a1bb46e284285fc4bbc47d6135
Amantina Sosa <amantinasosa@gmail.com>
2020-03-30 13:05:30 -0400

    show code mirror button works

--- client/components/ShowCodeMirror.js
+++ client/components/ShowCodeMirror.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Button } from 'primereact/button';
+import { toggleCodeMirror } from '../store/showCodeMirror';
 
 class ShowCodeMirror extends Component {
+  constructor(props) {
+    super(props);
+    this.toggleCode = this.toggleCode.bind(this);
+  }
+  toggleCode() {
+    console.log('test', this.props.toggleCodeMirror);
+    this.props.toggle();
+  }
+
   render() {
+    const label = this.props.showCodeMirror
+      ? 'Hide Code Mirror'
+      : 'Show Code Mirror';
     return (
       <div>
         <Button
           className="p-button-info p-button-raised"
-          label="Show Code Mirror"
+          label={label}
+          onClick={this.toggleCode}
         />
       </div>
     );
@@ -16,7 +30,17 @@ class ShowCodeMirror extends Component {
 }
 
 const mapStateToProps = state => {
-  return {};
+  return {
+    showCodeMirror: state.showCodeMirror,
+  };
 };
 
-export default connect(mapStateToProps)(ShowCodeMirror);
+const mapDispatchToProps = dispatch => {
+  console.log('here', toggleCodeMirror);
+
+  return {
+    toggle: show => dispatch(toggleCodeMirror(show)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShowCodeMirror);
