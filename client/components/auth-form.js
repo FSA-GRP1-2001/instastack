1d445427885549f4689946b45be7490e49392b77
Amantina Sosa <amantinasosa@gmail.com>
2020-03-31 14:53:43 -0400

    adding css for login

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -1,37 +1,217 @@
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import PropTypes from 'prop-types';
+// import { auth } from '../store';
+
+// /**
+//  * COMPONENT
+//  */
+// const AuthForm = props => {
+//   const { name, displayName, handleSubmit, error } = props;
+
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit} name={name}>
+//         <div>
+//           <label htmlFor="email">
+//             <small>Email</small>
+//           </label>
+//           <input name="email" type="text" />
+//         </div>
+//         <div>
+//           <label htmlFor="password">
+//             <small>Password</small>
+//           </label>
+//           <input name="password" type="password" />
+//         </div>
+//         <div>
+//           <button type="submit">{displayName}</button>
+//         </div>
+//         {error && error.response && <div> {error.response.data} </div>}
+//       </form>
+//       <a href="/auth/google">{displayName} with Google</a>
+//       <br />
+//       <a href="/auth/github">{displayName} with Github</a>
+//     </div>
+//   );
+// };
+
+// /**
+//  * CONTAINER
+//  *   Note that we have two different sets of 'mapStateToProps' functions -
+//  *   one for Login, and one for Signup. However, they share the same 'mapDispatchToProps'
+//  *   function, and share the same Component. This is a good example of how we
+//  *   can stay DRY with interfaces that are very similar to each other!
+//  */
+// const mapLogin = state => {
+//   return {
+//     name: 'login',
+//     displayName: 'Login',
+//     error: state.user.error,
+//   };
+// };
+
+// const mapSignup = state => {
+//   return {
+//     name: 'signup',
+//     displayName: 'Sign Up',
+//     error: state.user.error,
+//   };
+// };
+
+// const mapDispatch = dispatch => {
+//   return {
+//     handleSubmit(evt) {
+//       evt.preventDefault();
+//       const formName = evt.target.name;
+//       const email = evt.target.email.value;
+//       const password = evt.target.password.value;
+//       dispatch(auth(email, password, formName));
+//     },
+//   };
+// };
+
+// export const Login = connect(mapLogin, mapDispatch)(AuthForm);
+// export const Signup = connect(mapSignup, mapDispatch)(AuthForm);
+
+// /**
+//  * PROP TYPES
+//  */
+// AuthForm.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   displayName: PropTypes.string.isRequired,
+//   handleSubmit: PropTypes.func.isRequired,
+//   error: PropTypes.object,
+// };
+
+//=================================================================
+
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { auth } from '../store';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        InstaStack
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    // marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    // marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 /**
  * COMPONENT
  */
 const AuthForm = props => {
   const { name, displayName, handleSubmit, error } = props;
+  const classes = useStyles();
 
   return (
-    <div>
-      <form onSubmit={handleSubmit} name={name}>
-        <div>
-          <label htmlFor="email">
-            <small>Email</small>
-          </label>
-          <input name="email" type="text" />
+    <div className="signinbackground">
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Welcome
+          </Typography>
+          <form onSubmit={handleSubmit} name={name}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="Inprimary"
+              className={classes.submit}
+            >
+              {displayName}
+            </Button>
+            {error && error.response && <div> {error.response.data} </div>}
+
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/signup" variant="body2">
+                  Don't have an account? Sign Up
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+          <a href="/auth/google">{displayName} with Google</a>
         </div>
-        <div>
-          <label htmlFor="password">
-            <small>Password</small>
-          </label>
-          <input name="password" type="password" />
-        </div>
-        <div>
-          <button type="submit">{displayName}</button>
-        </div>
-        {error && error.response && <div> {error.response.data} </div>}
-      </form>
-      <a href="/auth/google">{displayName} with Google</a>
-      <br />
-      <a href="/auth/github">{displayName} with Github</a>
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Container>
     </div>
   );
 };
