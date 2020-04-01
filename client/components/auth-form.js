17cb7f3353a211bb350b4f56f07d6a7c7667782d
Max Han <mwellh84@gmail.com>
2020-04-01 12:55:27 -0400

    remove unused components

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -2,31 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { auth } from '../store';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Link from '@material-ui/core/Link';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import stack from '../../public/stack.png';
 import github from '../../public/github.png';
 import google from '../../public/google.png';
 import { InputText } from 'primereact/inputtext';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        InstaStack
-      </Link>
-      {new Date().getFullYear()}
-    </Typography>
-  );
-}
 
 const useStyles = makeStyles(theme => ({
   paper: {
