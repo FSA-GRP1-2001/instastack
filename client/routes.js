f6079e7e29def2d2c12fddbfb990da7524c22807
Mercedes <mercedesm@gmail.com>
2020-04-02 17:54:25 -0400

    Logged in user lands on DnD

--- client/routes.js
+++ client/routes.js
@@ -32,16 +32,15 @@ class Routes extends Component {
       <Switch>
         {/* Routes placed here are available to all visitors */}
         <Route exact path="/" component={GuestHome} />
-
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-        <Route path="/mainpage" component={MainPage} />
+        <Route path="/mainPage" component={MainPage} />
         {/* this is breaking the code for logged in users */}
         <Route exact path="/" component={GuestHome} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
-            <Route exact path="/home" component={UserHome} />
+            <Route exact path="/mainPage" component={MainPage} />
             <Route exact path="/users/:id/profile" component={Profile} />
             <Route path="/users/:id/projects" component={ProfileProjects} />
             <Route
